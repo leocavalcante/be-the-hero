e34251df93ed31e71001fe1c7cacb57c1165f590
leocavalcante <lc@leocavalcante.com>
2020-03-23 20:36:25 -0300

    Hello, World!

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let app = warp::get()
+        .map(|| "Hello, World!");
+
+    warp::serve(app)
+        .run(([127, 0, 0, 1], 3333))
+        .await;
 }
