eda3b560981b6e03f7e7ca0f787f00645fe4ec8d
leocavalcante <lc@leocavalcante.com>
2020-03-23 20:41:23 -0300

    Hello, JSON!

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,16 @@
+use std::collections::HashMap;
+
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
     let app = warp::get()
-        .map(|| "Hello, World!");
+        .map(|| {
+            let mut body = HashMap::new();
+            body.insert("event", "OminiStack Week 11.0");
+            body.insert("user", "Leo Cavalcante");
+            warp::reply::json(&body)
+        });
 
     warp::serve(app)
         .run(([127, 0, 0, 1], 3333))
